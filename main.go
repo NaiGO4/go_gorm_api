4e71f9fd2b24129ab29cb7a1a2689a2174984836
Giancarlos Yanayaco <giancarlosyc04@gmail.com>
2025-05-25 20:44:51 -0500

    Creacion de modelos de base de datos y conexion a la base de datos

--- main.go
+++ main.go
@@ -3,11 +3,19 @@ package main
 import (
 	"net/http"
 
+	"github.com/NaiG04/go_gorm_restapi/db"
+	"github.com/NaiG04/go_gorm_restapi/models"
 	"github.com/NaiG04/go_gorm_restapi/routes"
 	"github.com/gorilla/mux"
 )
 
 func main() {
+
+	db.DBConnection()
+
+	db.DB.AutoMigrate(models.Task{})
+	db.DB.AutoMigrate(models.User{})
+
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", routes.HomeHandler)
